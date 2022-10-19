46a91279fc8bacb89a23362d41b8632cc06d5202
talha4t <mahmudtalha80@gmail.com>
2022-10-19 11:10:04 +0600

    Chapter 06

---
+++ Chapter_06_Structure_Union/6_2_solution01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    struct student {
+        int id;
+        char* name;
+    };
+
+    struct student one;
+
+    one.id = 1;
+    one.name = "Talha Mahmud";
+
+    printf("ID: %d\n", one.id);
+    printf("Name: %s\n", one.name);
+
+    return 0;
+}
