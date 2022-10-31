8ce67e63490c29234045135be86c04a87101a043
Talha Mahmud <mahmudtalha80@gmail.com>
2022-10-31 11:45:04 +0600

    recursion

--- Chapter_03_Recursion/4-9.c
+++ Chapter_03_Recursion/4-9.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 
-void recurse(int count)
-{
-    if (count > 5) {
+void recurse(int count) {
+    if (count > 5)
         return;
-    }
-
-    printf("count : %d\n", count);
-
-    recurse(count+1);
     
-    printf("count = %d\n", count);
+    printf("count: %d\n", count);
+
+    recurse(count + 1);
+
+    printf("Count = %d\n", count);
 }
-int main()
-{
+
+int main() {
+
     recurse(1);
 
     return 0;
 }
+
+// 1
+// 2
+// 3
+// 4
+// 5
+// 5
+// 4
+// 3
+// 2
+// 1
