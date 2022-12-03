234b89a3dc43b87c8601ab6a8c50014ff8a1e877
talha4t <mahmudtalha80@gmail.com>
2022-12-03 14:30:16 +0600

    introduce dynamic memory allocation

--- Chapter_07_More_Pointer/7-1.c
+++ Chapter_07_More_Pointer/7-1.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int mian() {
+int main() {
 
     int *marks;
     int i, n;
@@ -9,18 +8,16 @@ int mian() {
     printf("Please enter the number of students: ");
     scanf("%d", &n);
 
-    // now allocate memory
+    // now allocation memory 
+    marks = (int *) malloc(sizeof(int) * n);
 
-    marks = (int *)malloc(sizeof(int) * n);
-
-    printf("Enter the marks for each student: \n");
+    printf("Enter the marks for each students: ");
     for (int i = 0; i < n; i++) {
         scanf("%d", &marks[i]);
     }
 
-    printf("Now here you can see the values\n");
-
-    //no print the marks array
+    printf("Now here you can see the values:\n");
+    // now print the marks array
     for (int i = 0; i < n; i++) {
         printf("%d\n", marks[i]);
     }
