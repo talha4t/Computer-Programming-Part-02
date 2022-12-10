d269d86ba6fce47953a9edfbd6bd0152da2cbc7e
talha4t <mahmudtalha80@gmail.com>
2022-12-10 11:50:10 +0600

    Recursion & Update

--- Chapter_04_Recursion/4-04-static-variable-scope.c
+++ Chapter_04_Recursion/4-04-static-variable-scope.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-void func() {
-
+void func()
+{
     int a = 10;
     static int s = 10;
 
     a = a + 2;
     s = s + 2;
+
+    printf("a = %d, s = %d\n", a, s);
 }
 
-int main() {
-
+int main()
+{
     func();
     func();
     func();
-
-    return 0;
 }
