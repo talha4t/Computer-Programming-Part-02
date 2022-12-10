d7c3081837041575b778e53fea630e775b321e03
talha4t <mahmudtalha80@gmail.com>
2022-12-10 21:11:32 +0600

    Chapter 09 Extra

---
+++ Chapter_09_Extra/9-04-macro-swap-using-bit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define SWAP(a, b){     \
+            a = a ^ b;  \
+            b = b ^ a;  \
+            a = a ^ b;  \
+    }
+    
+int main() 
+{
+    int a = 83, b = 323;
+
+    printf("a = %d, b = %d\n", a, b);
+
+    SWAP(a, b);
+
+    printf("a = %d, b = %d\n", a, b);
+
+    return 0;
+}
